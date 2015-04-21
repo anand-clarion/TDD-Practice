0401aa27527b4225f0f12b42e27e2b5e3b06ef86
kaushal-clarion <kaushal.kishor@clariontechnologies.co.in>
2015-04-21 20:18:04 +0530

    Added factories and exaple of use

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { expect(true).to be_truthy }
+
+  describe "use of factory-girl" do
+    context "When we create an user with factory" do
+      it "should add a new record in users table" do
+        expect { create(:user) } .to change { User.count }.by(1)
+      end
+    end
+  end
 end
