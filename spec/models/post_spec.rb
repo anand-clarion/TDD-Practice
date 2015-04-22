23c39460fb509f49f7c3b2853f015be62025509d
kaushal-clarion <kaushal.kishor@clariontechnologies.co.in>
2015-04-22 11:39:02 +0530

    Added Post model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,21 @@ describe Post do
   let(:post) { FactoryGirl.create(:post) }
 
   it { should respond_to(:title) }
-  it { should respond_to(:content)}
+  it { should respond_to(:content) }
+
+  describe "creation" do
+    context "valid attributes" do
+      it "should be valid" do
+        post = FactoryGirl.build(:post)
+        post.should be_valid
+      end
+    end
+
+    context "invalid attributes" do
+      it "should not be valid" do
+        post = FactoryGirl.build(:post, title: "", content: "")
+        post.should_not be_valid
+      end
+    end
+  end
 end
